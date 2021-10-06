882ed9c2197f7d76ddefb8290e06a4a93fad5435
Blaine Landowski <blalan05@gmail.com>
2021-10-06 11:17:00 -0500

    remove unnecessary plugin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,14 +2,5 @@ module.exports = {
   plugins: {
     autoprefixer: {},
     tailwindcss: {},
-    [
-      "postcss-preset-env",
-      {
-        stage: 3,
-        features: {
-          "nesting-rules": true
-        }
-      }
-    ]
-  },
+  }
 }
